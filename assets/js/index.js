3e149fe40e946d508808408104a09d77b8c84084
Jorge Espinoza <espinozadiazjorgeluis@gmail.com>
2022-11-08 23:01:59 -0500

    casi por terminar

---
+++ assets/js/index.js
@@ -0,0 +1,45 @@
+window.sr = ScrollReveal();
+
+
+sr.reveal('.home__info', {
+    duration: 3000,
+    origin: 'right',
+    distance: '-1000px'
+
+});
+
+
+
+sr.reveal('.home__image', {
+    duration: 3000,
+    origin: 'left',
+    distance: '-1000px'
+
+});
+
+
+sr.reveal('.Home', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+});
+
+
+sr.reveal('.about', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-100px'
+});
+
+sr.reveal('.about__card', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+});
+
+
+sr.reveal('.projects__project', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+});
